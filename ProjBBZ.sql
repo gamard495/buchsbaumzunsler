3ab4e171bce08b877e7710c074e6865aaf36af6b
Geovanny <geoconsultores.eu@gmail.com>
2022-01-16 09:01:00 -0500

    Beginn des Projekts

---
+++ ProjBBZ.sql
@@ -0,0 +1,4 @@
+-- Start des Projekts Umweltmonitoring des Buchsbaumzünslers in städtischen und ländlichen Gebieten Deutschlands unter Verwendung der freien Software Postgresql mit der PostGIS-Erweiterung.
+-- Erstellung der PostGIS-Erweiterung für die deutschland-Datenbank:
+
+CREATE EXTENSION postgis;
