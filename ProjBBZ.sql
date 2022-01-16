a64baea5dfe78b28c14d01a061454b835d1259b2
Geovanny <geoconsultores.eu@gmail.com>
2022-01-16 09:12:29 -0500

    Erstellung von Spalten für Bundesländer und Bezirke in der Datenbank für Buchsbaum und Buchsbaumzünsler.

--- ProjBBZ.sql
+++ ProjBBZ.sql
@@ -2,3 +2,11 @@
 -- Erstellung der PostGIS-Erweiterung für die deutschland-Datenbank:
 
 CREATE EXTENSION postgis;
+
+-- Erstellen Sie Spalten für Bundesländer und Bezirke für die PostGIS-Ebenen GBIF-Einträge für Buchsbaum und Buchsbaumzünsler, die Sie zuvor hinzugefügt haben.
+
+ALTER TABLE buchsbaumzunsler ADD COLUMN bundesland varchar(30);
+ALTER TABLE buchsbaum ADD COLUMN bundesland varchar(30);
+
+ALTER TABLE buchsbaumzunsler ADD COLUMN landkreis varchar(40);
+ALTER TABLE buchsbaum ADD COLUMN landkreis varchar(40);
